383680d83a9a33e05136fd86cd44785cde31abbb
Schooi <118210923+SchooiCodes@users.noreply.github.com>
2024-07-03 20:09:14 +0300

    Create server.js

---
+++ server.js
@@ -0,0 +1,35 @@
+const http = require('http');
+const url = require('url');
+const fs = require('fs');
+const path = require('path');
+
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  const parsedUrl = url.parse(req.url);
+  let pathname = `.${parsedUrl.pathname}`;
+
+  fs.stat(pathname, (err, stats) => {
+    if (err) {
+      res.statusCode = 404;
+      res.end(`File ${pathname} not found!`);
+    } else if (stats.isDirectory()) {
+      fs.readdir(pathname, (err, files) => {
+        if (err) {
+          res.statusCode = 500;
+          res.end(err.message);
+        } else {
+          res.setHeader('Content-Type', 'application/json');
+          res.end(JSON.stringify(files.filter(file => fs.statSync(path.join(pathname, file)).isDirectory())));
+        }
+      });
+    } else {
+      res.statusCode = 404;
+      res.end(`Path ${pathname} is not a directory!`);
+    }
+  });
+});
+
+server.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}/`);
+});
